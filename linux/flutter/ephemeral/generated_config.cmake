5e925f0083c9b2a995d84dbd3c917be678ea31b7
Akeel Mofeed <akeelmofeed@192.168.1.5>
2025-06-27 11:09:56 +0300

    Deploy website

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/akeelmofeed/dev/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/akeelmofeed/Documents/Sutoory/sutoory_website" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/akeelmofeed/dev/flutter"
+  "PROJECT_DIR=/home/akeelmofeed/Documents/Sutoory/sutoory_website"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NmZiYTI0NDdlOQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049OGNkMTllNTA5ZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/akeelmofeed/Documents/Sutoory/sutoory_website/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/akeelmofeed/Documents/Sutoory/sutoory_website/lib/main.dart"
+)
